ac647216287a3efc0e2a132fb3a61b7087885c0c
raredeveloperofficial <max2d12345@gmail.com>
2025-04-10 16:47:52 +0530

    component update 1

--- app/src/main/java/com/raredeveloper/rareengine2/RareEngine2/GameUtils/Component.java
+++ app/src/main/java/com/raredeveloper/rareengine2/RareEngine2/GameUtils/Component.java
@@ -2,13 +2,17 @@ package RareEngine2.GameUtils;
 
 public class Component
 {
+	//this is main component that anyone can make their own by extending it
 	public boolean isEnabled = true;
+	//called when at start
 	public void start(GameObject o,GameView gv){
 		
 	}
+	//called before frame draw
 	public void update(GameObject o,GameView gv){
 		
 	}
+	//on destroy
 	public void destroy(GameObject o,GameView gv){
 		
 	}
