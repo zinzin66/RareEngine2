93b544b8f7f2cc0605fe5991cf76136aa3f05421
raredeveloperofficial <max2d12345@gmail.com>
2025-04-10 15:58:29 +0530

    clearer1

--- app/src/main/java/com/raredeveloper/rareengine2/MainActivity.java
+++ app/src/main/java/com/raredeveloper/rareengine2/MainActivity.java
@@ -11,36 +11,18 @@ import android.content.ClipboardManager;
 
 public class MainActivity extends Activity 
 {
-	public static Context ct;
+	//this is example activity show how rare engine works
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
 		GameView gv = new GameView(this,this);
         setContentView(gv);
-		MainActivity.ct = this;
-		
-		try{
 		GameObject o = new GameObject();
 		ImageRenderer renderer = new ImageRenderer();
 		renderer.setImage(renderer.getFromResource(getResources(),R.drawable.ic_launcher));
 		o.addComponent(renderer,gv);
 		o.addComponent(new ObjectScript(),gv);
 		gv.addObject(o);
-		}catch(Exception e){
-			String exceptionMessage = e.getMessage();
-
-			// Get the ClipboardManager system service
-			ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-
-			// Copy the exception message to the clipboard
-			if (clipboard != null) {
-				android.content.ClipData clip = android.content.ClipData.newPlainText("Exception Message", exceptionMessage);
-				clipboard.setPrimaryClip(clip);
-
-				// Show a toast message to inform the user
-				Toast.makeText(this, "Exception copied to clipboard", Toast.LENGTH_SHORT).show();
-			}
-		}
     }
 }
